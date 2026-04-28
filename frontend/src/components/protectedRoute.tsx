6135a7a66b2fe1b1b70f819d7e5c13570e761b38
mohd Adil <mohdadil.tech@gmail.com>
2026-04-28 22:39:08 +0530

    Protected Route

---
+++ frontend/src/components/protectedRoute.tsx
@@ -0,0 +1,23 @@
+ 
+
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import { UseAppData } from '../context/AppContext';
+
+const ProtectedRoute = () => {
+    const { isAuth, user, loading } = UseAppData();
+
+    if (loading) return null;
+    const location = useLocation();
+
+    if (!isAuth) {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (user?.role === null && location.pathname !== "/select-role") {
+        return <Navigate to="/select-role" replace />;
+    }
+
+    return <Outlet />;
+}
+
+export default ProtectedRoute;
